cf786a9511d46ca4f887ecd9ba4803d995cb36de
USMortality.com <USMortality@protonmail.com>
2023-03-23 21:01:59 -0700

    add offending code

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import VueSlider from 'vue-slider-component'
+import 'vue-slider-component/theme/default.css'
+
 import './assets/main.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.component('VueSlider', VueSlider)
+
+app.mount('#app')
